4dc199dc52648adc84f032b6d2dfa1a02d55c877
amarachigoodness74 <amarachigoodness74@gmail.com>
2025-02-21 06:25:28 +0100

    feature to add finace data completed

--- expense-tracker/main.py
+++ expense-tracker/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import csv
 from datetime import datetime
+from data_entry import get_date, get_amount, get_category, get_description
 
 class CSV :
   CSV_FILE = "finance_data.csv"
@@ -27,5 +28,12 @@ class CSV :
       writer.writerow(new_entry)
     print("Entry added successfully")
 
-CSV.initialize_csv()
-CSV.add_entry("2025-01-25", 125.87, "Income", "To buy data")
+def add_finance_data():
+  CSV.initialize_csv()
+  date = get_date("Enter the date of the transaction (dd-mm-yyyy) or press 'Enter' for today's date: ", allow_default=True)
+  amount = get_amount()
+  category = get_category()
+  description = get_description()
+  CSV.add_entry(date, amount, category, description)
+
+add_finance_data()
